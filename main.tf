c16953a889d3fe530d00aaa5b1e7ce51755db952
Ethan Davis <wetheethan@gmail.com>
2020-03-18 16:25:31 -0500

    Setup secrethub to securely pass secrets through terraform

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = var.region
 }
 
+provider "secrethub" {
+  credential = file("~/.secrethub/credential")
+}
+
 terraform {
   backend "s3" {
     bucket = "poc-terraformstate"
@@ -13,6 +17,24 @@ terraform {
 data "aws_caller_identity" "current" {}
 
 
+locals {
+  secrethub_dir = "tirgus-api/poc"
+}
+
+resource "secrethub_secret" "db_password" {
+  path = "${local.secrethub_dir}/db/password"
+
+  generate {
+    length      = 22
+    use_symbols = true
+  }
+}
+
+resource "secrethub_secret" "db_user" {
+  path  = "${local.secrethub_dir}/db/user"
+  value = "schema_user"
+}
+
 resource "aws_db_instance" "postgres_instance" {
   allocated_storage = 20
   storage_type = "gp2"
@@ -21,8 +43,8 @@ resource "aws_db_instance" "postgres_instance" {
   instance_class = "db.t2.micro"
   name = "tirgusApiDb"
   identifier = "tirgus-api-db"
-  username = "ENTER_USER"
-  password = "ENTER_PASSWORD"
+  username = secrethub_secret.db_user.value
+  password =  secrethub_secret.db_password.value
   parameter_group_name = aws_db_parameter_group.postgres_db.name
 }
 
